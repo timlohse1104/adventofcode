e2df9c40f300889cc48a9c9c246dc3bba0863d42
timlohse1104 <tim-lohse@gmx.de>
2022-12-01 22:40:37 +0100

    Added far better solution

--- 2022/0112/solution.js
+++ 2022/0112/solution.js
@@ -2,17 +2,29 @@ import * as fs from 'fs/promises';
 
 const input = await fs.readFile('input.txt', 'utf-8');
 
-//Bad solution
-const lines = input.split('\n');
-let foo = [];
-let fooSum = 0;
-for (let i = 0; i < lines.length; i++) {
-  if (lines[i]) {
-    fooSum += Number(lines[i]);
-  } else {
-    foo.push(fooSum);
-    fooSum = 0;
-  }
-}
-console.log(foo.sort().reverse()[0]);
+// Bad solution
+// const lines = input.split('\n');
+// let foo = [];
+// let fooSum = 0;
+// for (let i = 0; i < lines.length; i++) {
+//   if (lines[i]) {
+//     fooSum += Number(lines[i]);
+//   } else {
+//     foo.push(fooSum);
+//     fooSum = 0;
+//   }
+// }
+//console.log(foo.sort().reverse()[0]);
+
+// Good solution
+console.log(
+  Math.max(
+    ...input.split('\n\n').map((line) =>
+      line
+        .split('\n')
+        .map((s) => Number(s))
+        .reduce((a, c) => a + c, 0)
+    )
+  )
+);
 
