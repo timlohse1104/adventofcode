c14cbde961440f08346a34600a5b8c10af0e3a4d
timlohse1104 <tim-lohse@gmx.de>
2022-12-04 23:38:56 +0100

    Added solution for day 3 bonus puzzle

--- 2022/0312/solution.js
+++ 2022/0312/solution.js
@@ -1,6 +1,6 @@
-import * as fs from 'fs/promises';
+import * as fs from "fs/promises";
 
-const input = await fs.readFile('input.txt', 'utf-8');
+const input = await fs.readFile("input.txt", "utf-8");
 
 // priorities: a-z -> 1-26
 // const small = 'abcdefghijklmnopqrstuvwxyz';
@@ -9,25 +9,46 @@ const input = await fs.readFile('input.txt', 'utf-8');
 // const large = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 // Array.from(large).map((char) => console.log(char, char.charCodeAt(0), 'priority: ', char.charCodeAt(0) - 38));
 
-const getPrio = (c) => (c.charCodeAt(0) > 96 ? c.charCodeAt(0) - 96 : c.charCodeAt(0) - 38);
+const getPrio = (c) =>
+  c.charCodeAt(0) > 96 ? c.charCodeAt(0) - 96 : c.charCodeAt(0) - 38;
 
+// new Set create a collection if unique values: https://www.geeksforgeeks.org/sets-in-javascript/
 const calculateDuplicateValue = (l) => {
-  console.log(l[0], l[1]);
-  // new Set create a collection if unique values: https://www.geeksforgeeks.org/sets-in-javascript/
-  const sumOfLine = [...new Set(l[0])].reduce((a, c) => {
-    console.log(c, l[1].includes(c), getPrio(c));
-    return a + (l[1].includes(c) ? getPrio(c) : 0);
+  return [...new Set(l[0])].reduce((a, c) => {
+    return (
+      a +
+      ((
+        l.length === 3 ? l[1].includes(c) && l[2].includes(c) : l[1].includes(c)
+      )
+        ? getPrio(c)
+        : 0)
+    );
   }, 0);
-  console.log(sumOfLine);
-  return sumOfLine;
 };
 
 console.log(
   input
-    .split('\n')
-    .map((line) => `${line.slice(0, line.length / 2)},${line.slice(line.length / 2)}`.split(','))
+    .split("\n")
+    .map((line) =>
+      `${line.slice(0, line.length / 2)},${line.slice(line.length / 2)}`.split(
+        ","
+      )
+    )
     .reduce((a, c) => a + calculateDuplicateValue(c), 0)
 );
 
 // Bonus
-
+const lines = input.split("\n");
+let tempArr = [];
+const result = lines.map((line) => {
+  if (tempArr.length === 3) tempArr = [];
+  tempArr.push(line);
+  if (tempArr.length === 3) return tempArr;
+});
+console.log(
+  "Bonus: ",
+  result
+    .filter((l) => l)
+    .map((l) => calculateDuplicateValue(l))
+    .reduce((a, c) => a + c)
+);
