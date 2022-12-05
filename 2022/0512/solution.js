e38f8a3bd2d4b68a6fdd2b999fa7e5166918c099
timlohse1104 <tim-lohse@gmx.de>
2022-12-05 18:12:01 +0100

    Added solution for day 5 bonus puzzle

--- 2022/0512/solution.js
+++ 2022/0512/solution.js
@@ -14,7 +14,6 @@ const stacks = [
 ];
 
 const makeMove = ([amount, start, target]) => {
-  console.log(`Move ${amount} from ${start} to ${target}`);
   for (let i = 0; i < amount; i++) {
     stacks[target - 1].push(stacks[start - 1].pop());
   }
@@ -24,6 +23,29 @@ moves
   .split('\n')
   .filter((move) => move.match(/m+/))
   .map((move) => move.replace('move ', '').replace(' from ', ',').replace(' to ', ',').split(','))
-  .forEach((move, i) => makeMove(move));
+  .forEach((move) => makeMove(move));
 console.log(stacks.map((stack) => stack[stack.length - 1]).join(''));
+
+// Bonus
+const bonusStacks = [
+  ['L', 'N', 'W', 'T', 'D'],
+  ['C', 'P', 'H'],
+  ['W', 'P', 'H', 'N', 'D', 'G', 'M', 'J'],
+  ['C', 'W', 'S', 'N', 'T', 'Q', 'L'],
+  ['P', 'H', 'C', 'N'],
+  ['T', 'H', 'N', 'D', 'M', 'W', 'Q', 'B'],
+  ['M', 'B', 'R', 'J', 'G', 'S', 'L'],
+  ['Z', 'N', 'W', 'G', 'V', 'B', 'R', 'T'],
+  ['W', 'G', 'D', 'N', 'P', 'L'],
+];
+const makeBonusMove = ([amount, start, target]) => {
+  bonusStacks[target - 1] = [...bonusStacks[target - 1], ...bonusStacks[start - 1].splice(-amount)];
+};
+
+moves
+  .split('\n')
+  .filter((move) => move.match(/m+/))
+  .map((move) => move.replace('move ', '').replace(' from ', ',').replace(' to ', ',').split(','))
+  .forEach((move) => makeBonusMove(move));
+console.log(bonusStacks.map((stack) => stack[stack.length - 1]).join(''));
 
