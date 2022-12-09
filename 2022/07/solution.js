d4101cd12277657cf222f5e295a3662b82135b43
timlohse1104 <tim-lohse@gmx.de>
2022-12-09 19:14:36 +0100

    Added correct solution for puzzle day 7

--- 2022/07/solution.js
+++ 2022/07/solution.js
@@ -4,28 +4,34 @@ import * as fs from 'fs/promises';
 
 const input = await fs.readFile('input.txt', 'utf-8');
 
-// Solution for https://adventofcode.com/2022/day/6
+// Solution for https://adventofcode.com/2022/day/7
 
 console.log(
   input
     .split('\n')
     .map((l, i, a) => {
-      const dir = { name: '', size: 0, content: [] };
-      if (/^\$\s(cd)\s\w+/.test(l)) {
+      const ಠ_ಠ = { size: '', size: 0, dirs: [], files: [] };
+      if (/^\$\s(cd)\s(\w+)|\//.test(l)) {
         let j = i + 2;
-        dir.name = l.replace('$ cd ', '');
+        ಠ_ಠ.size = l.replace('$ cd ', '');
         while (!/^\$.+/.test(a[j]) && a[j]) {
-          dir.content.push({ type: /^\d+/.test(a[j]) ? 'file' : 'dir', size: /^\d+/.test(a[j]) ? Number(a[j].replace(/\s\w+.\w+/, '')) : 0 });
+          const e = {
+            type: /^\d+/.test(a[j]) ? 'file' : 'dir',
+            name: a[j].replace(/^dir\s+/, ''),
+            size: /^\d+/.test(a[j]) ? Number(a[j].replace(/\s\w+.\w+/, '')) : 0,
+          };
+          /^\d+/.test(a[j]) ? ಠ_ಠ.files.push(e) : ಠ_ಠ.dirs.push(e);
           j++;
         }
       }
-      dir.size = dir.content.reduce((a, c) => a + c.size, 0);
-      return dir;
+      ಠ_ಠ.size = ಠ_ಠ.files.reduce((a, c) => a + c.size, 0);
+      return ಠ_ಠ;
     })
     .filter((l) => l.name !== '')
+    .reverse()
     .map((d, i, a) => {
-      d.content.forEach((c) => {
-        if (c.type === 'dir') d.size += a.find((e) => e.name === d.name)?.size;
+      d.dirs.forEach((c) => {
+        d.size += a.find((e) => e.name === c.name)?.size;
       });
       return d;
     })
