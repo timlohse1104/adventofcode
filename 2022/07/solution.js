7e60ea5fffa3dd9912c0c087d580ead368426710
timlohse1104 <tim-lohse@gmx.de>
2022-12-10 11:47:22 +0100

    Added attempt to solve day 7 bonus puzzle

--- 2022/07/solution.js
+++ 2022/07/solution.js
@@ -6,35 +6,44 @@ const input = await fs.readFile('input.txt', 'utf-8');
 
 // Solution for https://adventofcode.com/2022/day/7
 
-console.log(
-  input
-    .split('\n')
-    .map((l, i, a) => {
-      const ಠ_ಠ = { name: '', size: 0, dirs: [], files: [] };
-      if (/^\$\s(cd)\s(\w+)|\//.test(l)) {
-        let j = i + 2;
-        ಠ_ಠ.name = l.replace('$ cd ', '');
-        while (!/^\$.+/.test(a[j]) && a[j]) {
-          const e = {
-            type: /^\d+/.test(a[j]) ? 'file' : 'dir',
-            name: a[j].replace(/^dir\s+/, ''),
-            size: /^\d+/.test(a[j]) ? Number(a[j].replace(/\s\w+.\w+/, '')) : 0,
-          };
-          /^\d+/.test(a[j]) ? ಠ_ಠ.files.push(e) : ಠ_ಠ.dirs.push(e);
-          j++;
-        }
+const folders = input
+  .split('\n')
+  .map((l, i, a) => {
+    const ಠ_ಠ = { name: '', size: 0, dirs: [], files: [] };
+    if (/^\$\s(cd)\s(\w+)|\//.test(l)) {
+      let j = i + 2;
+      ಠ_ಠ.name = l.replace('$ cd ', '');
+      while (!/^\$.+/.test(a[j]) && a[j]) {
+        const e = {
+          type: /^\d+/.test(a[j]) ? 'file' : 'dir',
+          name: a[j].replace(/^dir\s+/, ''),
+          size: /^\d+/.test(a[j]) ? Number(a[j].replace(/\s\w+.\w+/, '')) : 0,
+        };
+        /^\d+/.test(a[j]) ? ಠ_ಠ.files.push(e) : ಠ_ಠ.dirs.push(e);
+        j++;
       }
-      ಠ_ಠ.size = ಠ_ಠ.files.reduce((a, c) => a + c.size, 0);
-      return ಠ_ಠ;
-    })
-    .filter((l) => l.name !== '')
-    .reverse()
-    .map((d, i, a) => {
-      d.dirs.forEach((c) => {
-        d.size += a.find((e) => e.name === c.name)?.size;
-      });
-      return d;
-    })
-    .reduce((a, c) => a + (c.size <= 100000 ? c.size : 0), 0)
-);
+    }
+    ಠ_ಠ.size = ಠ_ಠ.files.reduce((a, c) => a + c.size, 0);
+    return ಠ_ಠ;
+  })
+  .filter((l) => l.name !== '')
+  .reverse()
+  .map((d, i, a) => {
+    d.dirs.forEach((c) => {
+      d.size += a.find((e) => e.name === c.name)?.size;
+    });
+    return d;
+  });
+console.log(folders.reduce((a, c) => a + (c.size <= 100000 ? c.size : 0), 0));
+
+// Bonus
+const sizes = input
+  .split('\n')
+  .filter((l) => /\d+/.test(l))
+  .map((l) => Number(l.replace(/\s\w+.\w+/, '')))
+  .filter((l) => l.name !== '');
+console.log('required', 30000000 - (70000000 - sizes.reduce((a, c) => a + c, 0)));
+console.log('Bonus', Math.min(...folders.filter((f) => f.size >= 30000000 - (70000000 - sizes.reduce((a, c) => a + c, 0))).map((e) => e.size)));
+
+// returns 8701334, correct answer is 7421137 - what am I missing?
 
