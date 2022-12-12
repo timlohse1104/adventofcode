813eb91e64edcffb5c1e1e8a3d9ab80b69947d80
timlohse1104 <tim-lohse@gmx.de>
2022-12-12 17:40:47 +0100

    Added math abs and made refinements

--- 2022/09/solution.js
+++ 2022/09/solution.js
@@ -1,14 +1,12 @@
-"use strict";
+'use strict';
 
-import * as fs from "fs/promises";
+import * as fs from 'fs/promises';
 
-const input = await fs.readFile("input.txt", "utf-8");
+const input = await fs.readFile('input.txt', 'utf-8');
 
 // Solution for https://adventofcode.com/2022/day/9
 
-const moves = input
-  .split("\n")
-  .map((m) => m.split(" ").map((e, i) => (i === 1 ? Number(e) : e)));
+const moves = input.split('\n').map((m) => m.split(' ').map((e, i) => (i === 1 ? Number(e) : e)));
 
 const headPos = { x: 0, y: 0 };
 let headHistoryPos = { x: 0, y: 0 };
@@ -17,52 +15,49 @@ const headHistory = [{ x: 0, y: 0 }];
 const tailHistory = [{ x: 0, y: 0 }];
 
 const calcDistance = () => {
-  let xDistance = headPos.x - tailPos.x;
-  let yDistance = headPos.y - tailPos.y;
-  return (
-    (xDistance < 0 ? -xDistance : xDistance) +
-    (yDistance < 0 ? -yDistance - 1 : yDistance - 1)
-  );
+  let xDistance = Math.abs(headPos.x - tailPos.x);
+  let yDistance = Math.abs(headPos.y - tailPos.y);
+  return xDistance + yDistance;
 };
 
 const makeMove = (type, direction) => {
-  if (type === "head") {
+  if (type === 'head') {
     switch (direction) {
-      case "U":
-        headHistoryPos = { ...headPos };
+      case 'U':
+        headHistoryPos = { x: headPos.x, y: headPos.y };
         headPos.y++;
-        headHistory.push(headPos);
+        headHistory.push({ x: headPos.x, y: headPos.y });
         break;
-      case "D":
-        headHistoryPos = { ...headPos };
+      case 'D':
+        headHistoryPos = { x: headPos.x, y: headPos.y };
         headPos.y--;
-        headHistory.push(headPos);
+        headHistory.push({ x: headPos.x, y: headPos.y });
         break;
-      case "L":
-        headHistoryPos = { ...headPos };
+      case 'L':
+        headHistoryPos = { x: headPos.x, y: headPos.y };
         headPos.x--;
-        headHistory.push(headPos);
+        headHistory.push({ x: headPos.x, y: headPos.y });
         break;
-      case "R":
-        headHistoryPos = { ...headPos };
+      case 'R':
+        headHistoryPos = { x: headPos.x, y: headPos.y };
         headPos.x++;
-        headHistory.push(headPos);
+        headHistory.push({ x: headPos.x, y: headPos.y });
     }
-  } else if (type === "tail") {
-    tailPos = { ...headHistoryPos };
-    tailHistory.push(tailPos);
+  } else if (type === 'tail') {
+    tailPos = { x: headHistoryPos.x, y: headHistoryPos.y };
+    tailHistory.push({ x: tailPos.x, y: tailPos.y });
   }
 };
 
 moves.forEach((m) => {
   if (m[1] > 1) {
     for (let i = 1; i <= m[1]; i++) {
-      makeMove("head", m[0]);
-      calcDistance() > 1 ? makeMove("tail") : 0;
+      makeMove('head', m[0]);
+      if (calcDistance() > 1) makeMove('tail');
     }
   } else {
-    makeMove("head", m[0]);
-    calcDistance() > 1 ? makeMove("tail") : 0;
+    makeMove('head', m[0]);
+    if (calcDistance() > 1) makeMove('tail');
   }
 });
 
@@ -75,4 +70,5 @@ const uniqueTailHistory = tailHistory.filter((entry) => {
   xY.add(entry.x, entry.y);
   return true;
 });
-console.log("unique", uniqueTailHistory.length);
+console.log('unique', uniqueTailHistory.length);
+
