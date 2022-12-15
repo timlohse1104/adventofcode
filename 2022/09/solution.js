2a58d8ae44d2464891ed1ed5253e64711a8cab6f
timlohse1104 <tim-lohse@gmx.de>
2022-12-15 23:29:00 +0100

    Tried fixing day 9 solution

--- 2022/09/solution.js
+++ 2022/09/solution.js
@@ -8,56 +8,34 @@ const input = await fs.readFile('input.txt', 'utf-8');
 
 const moves = input.split('\n').map((m) => m.split(' ').map((e, i) => (i === 1 ? Number(e) : e)));
 
-const headPos = { x: 0, y: 0 };
-let headHistoryPos = { x: 0, y: 0 };
-let tailPos = { x: 0, y: 0 };
+let head = { x: 0, y: 0 };
+let tail = { x: 0, y: 0 };
 const headHistory = [{ x: 0, y: 0 }];
 const tailHistory = [{ x: 0, y: 0 }];
 
-const calcDistance = () => {
-  let xDistance = Math.abs(headPos.x - tailPos.x);
-  let yDistance = Math.abs(headPos.y - tailPos.y);
-  return xDistance + yDistance;
+const getDistance = (lead, follow) => {
+  return { x: Math.abs(lead.x - follow.x), y: Math.abs(lead.y - follow.y) };
 };
 
-const makeMove = (type, direction) => {
-  if (type === 'head') {
-    switch (direction) {
-      case 'U':
-        headHistoryPos = { x: headPos.x, y: headPos.y };
-        headPos.y++;
-        headHistory.push({ x: headPos.x, y: headPos.y });
-        break;
-      case 'D':
-        headHistoryPos = { x: headPos.x, y: headPos.y };
-        headPos.y--;
-        headHistory.push({ x: headPos.x, y: headPos.y });
-        break;
-      case 'L':
-        headHistoryPos = { x: headPos.x, y: headPos.y };
-        headPos.x--;
-        headHistory.push({ x: headPos.x, y: headPos.y });
-        break;
-      case 'R':
-        headHistoryPos = { x: headPos.x, y: headPos.y };
-        headPos.x++;
-        headHistory.push({ x: headPos.x, y: headPos.y });
-    }
-  } else if (type === 'tail') {
-    tailPos = { x: headHistoryPos.x, y: headHistoryPos.y };
-    tailHistory.push({ x: tailPos.x, y: tailPos.y });
-  }
+const move = (node, nodeHistory, directionString, directionNode) => {
+  const directions = { U: { x: 0, y: 1 }, D: { x: 0, y: -1 }, L: { x: -1, y: 0 }, R: { x: 1, y: 0 } };
+  node.x += directionNode ? directionNode.x : directions[directionString].x;
+  node.y += directionNode ? directionNode.y : directions[directionString].y;
+  nodeHistory.push({ ...node });
 };
 
 moves.forEach((m) => {
+  console.log(m);
   if (m[1] > 1) {
     for (let i = 1; i <= m[1]; i++) {
-      makeMove('head', m[0]);
-      if (calcDistance() > 1) makeMove('tail');
+      move(head, headHistory, m[0]);
+      const distanceNode = getDistance(head, tail);
+      if (distanceNode.x > 1 || distanceNode.y > 1) move(tail, tailHistory, '', distanceNode);
     }
   } else {
-    makeMove('head', m[0]);
-    if (calcDistance() > 1) makeMove('tail');
+    move(head, headHistory, m[0]);
+    const distanceNode = getDistance(head, tail);
+    if (distanceNode.x > 1 || distanceNode.y > 1) move(tail, tailHistory, '', distanceNode);
   }
 });
 
