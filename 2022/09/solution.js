4a6d15e97da5c0e33238a5e737554faa99b93481
timlohse1104 <tim-lohse@gmx.de>
2022-12-18 21:04:52 +0100

    Added correct solution for day 9 puzzle

--- 2022/09/solution.js
+++ 2022/09/solution.js
@@ -18,29 +18,28 @@ function calcTailHistory(movements) {
   let head = { x: 0, y: 0 };
   let tail = { x: 0, y: 0 };
   const headHistory = [{ x: 0, y: 0 }];
-  const tailHistory = [{ x: 0, y: 0 }];
+  const tailHistory = new Set();
+  tailHistory.add('x: 0, y: 0');
 
-  const getDistance = (lead, follow) => ({ x: Math.abs(lead.x - follow.x), y: Math.abs(lead.y - follow.y) });
+  const getDistance = (lead, follow) => ({ x: lead.x - follow.x, y: lead.y - follow.y });
 
   const makeMove = (direction) => {
     const directions = { U: { x: 0, y: 1 }, D: { x: 0, y: -1 }, L: { x: -1, y: 0 }, R: { x: 1, y: 0 } };
-    // Move head first
-    head.x += Math.sign(directions[direction].x);
-    head.y += Math.sign(directions[direction].y);
+    const { x, y } = directions[direction];
+    head.x += Math.sign(x);
+    head.y += Math.sign(y);
     headHistory.push({ ...head });
-    // Move tail according to distance between head and tail
-    if (Math.abs(head.x - tail.x) === 2) tail.x += Math.sign(directions[direction].x);
-    if (Math.abs(head.y - tail.y) === 2) tail.y += Math.sign(directions[direction].y);
-    tailHistory.push({ ...tail });
-
-    const distanceNode = getDistance(head, tail);
-    if (distanceNode.x > 1) moveX(tail, tailHistory, '', distanceNode);
-    if (distanceNode.y > 1) moveY(tail, tailHistory, '', distanceNode);
+    const distance = getDistance(head, tail);
+    if (Math.abs(distance.x) > 1 || Math.abs(distance.y) > 1) {
+      tail.x += Math.sign(distance.x);
+      tail.y += Math.sign(distance.y);
+      tailHistory.add(`x: ${tail.x}, y: ${tail.y}`);
+    }
   };
 
   for (const movement of movements) {
     if (movement.distance > 1) {
-      for (let i = 1; i <= movement.distance; i++) {
+      for (let i = 0; i < movement.distance; i++) {
         makeMove(movement.direction);
       }
     } else {
@@ -51,15 +50,4 @@ function calcTailHistory(movements) {
   return tailHistory;
 }
 const tailHistory = calcTailHistory(movements);
-
-console.log(tailHistory.length);
-const xY = new Set();
-const uniqueTailHistory = tailHistory.filter((entry) => {
-  if (xY.has(entry.x && entry.y)) {
-    return false;
-  }
-  xY.add(entry.x, entry.y);
-  return true;
-});
-console.log('unique', uniqueTailHistory.length);
-
+console.log(tailHistory.size);
