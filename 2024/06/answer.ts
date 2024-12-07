05586dbe334815d965080a65d4f8ee1305038b69
timlohse1104 <tim-lohse@gmx.de>
2024-12-07 21:36:52 +0100

    Added 2024 day 6 part one boilerplate and test input

---
+++ 2024/06/answer.ts
@@ -0,0 +1,27 @@
+const decoder = new TextDecoder("utf-8");
+const data = Deno.readFileSync(new URL("input.txt", import.meta.url).pathname);
+const input = decoder.decode(data);
+
+// Solution for https://adventofcode.com/2024/day/06
+
+const testInput = `....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...`;
+
+
+
+// Part 1
+const solution1 = '';
+
+// Part 2
+const solution2 = '';
+
+// Answer
+console.log([`${solution1}`, `${solution2}`]);
